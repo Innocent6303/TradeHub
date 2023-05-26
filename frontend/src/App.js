36cdfedf729a0cf2b067cabe02f7b6f83d71a6b8
unknown <Mujeeb9908>
2023-05-27 03:36:12 +0530

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <div>
       <div>App</div>
       <Header />
+      <Footer />
     </div>
   );
 }
